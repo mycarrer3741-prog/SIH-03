0102cd079592f3d3dcbbfd39c9eed50a9213c38c
mycarrer3741-prog <mycarrer3741@gmail.com>
2026-09-09 23:44:51 +0530

    Update service worker cache to latest website version

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
-const CACHE='mithra-birthday-v1';
-const ASSETS=['./','./index.html','./style.css?v=18','./script.js?v=11','./manifest.json','./birthday.mp3','./kumki 1.mp3','./break 1.mp4'];
+const CACHE='mithra-birthday-v2';
+const ASSETS=['./','./index.html','./style.css?v=21','./script.js?v=13','./manifest.json','./birthday.mp3','./kumki 1.mp3','./break 1.mp4'];
 self.addEventListener('install',e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS).catch(()=>{})).then(()=>self.skipWaiting()))});
 self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim()))});
 self.addEventListener('fetch',e=>{if(e.request.method!=='GET')return;e.respondWith(caches.match(e.request).then(cached=>cached||fetch(e.request).then(r=>{const copy=r.clone();caches.open(CACHE).then(c=>c.put(e.request,copy));return r}).catch(()=>caches.match('./index.html'))))});
